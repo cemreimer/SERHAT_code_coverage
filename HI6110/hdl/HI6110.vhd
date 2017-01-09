4561b255a1131e129a752652e8c1722b7835822b
cemreimer <cemre.imer1992@gmail.com>
2017-01-09 14:31:35 +0300

    Internal register handling
    Made amendment on I/Os. Determined a way of internal register handling.
    Error generation devised.

--- HI6110/hdl/HI6110.vhd
+++ HI6110/hdl/HI6110.vhd
@@ -17,9 +17,9 @@
 --------------------------------------------------------------------------------
 ----default libs----
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.all;
-use IEEE.std_logic_unsigned.all
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ----third party libs----
 use work.types.all;
@@ -37,37 +37,39 @@ port (
    PIMR       : in std_logic;
    POERROR    : out std_logic;
    POVALMESS  : out std_logic;
-   POFFEMPTY  : out std_logic;
-   PORFLAG    : out std_logic;
    PIBUSA     : in std_logic;
    PIBUSB     : in std_logic;     
    PIODATAWORD: inout word_vector(31 downto 0);
-   PICMD      : in std_logic_vector(15 downto 0);
+   PICMD      : in std_logic_vector(15 downto 0); 
    PORCVA     : out std_logic;
    PORCVB     : out std_logic; 
    PIRTA      : in std_logic;
-   PIRTAP     : in std_logic
+   PIRTAP     : in std_logic;
+   PIERR      : in std_logic_vector(8 DOWNTO 0)  ---- HI6110 error register tanimina bakin. 
 );
 end HI6110;
 
 architecture architecture_HI6110 of HI6110 is
 
 ----HI6110 Message and Register Buffering States----
-type states is (StIdle, StCSWait, StValidCmd, ) 
+type states is (StIdle, StBusActivity, StValidCmdCheck, StProcessCmd, StProcessData) 
 signal H6110_states: states;
 
 ----From FPGA to HI6110 ports----
 signal SMR, SRTA, SRTAP, SBCSTART : std_logic; 
 
 ----From HI6110 to FPGA IRQs----
-signal SRCVA, SRCVB, SError, SVALMESS, SFFEMPTY, SRFLAG: std_logic; 
+signal SRCVA, SRCVB, SError, SVALMESS, SFFEMPTY: std_logic; 
 
 ----RT Physical Address Parity Check Signal----
 signal SPrtyCheck: std_logic;
+signal SPRTYERR: std_logic;
 
 ----Bus Activity Detectors---
 signal SRcvFlags: std_logic_vector(1 downto 0);
 signal SBUSA, SBUSB: std_logic;
+signal SPrevBUSA, SPrevBUSB : std_logic;
+signal SActA, SActB: std_logic;
 
 ----Data FIFOs----
 signal STXFIFO  : word_vector(31 downto 0);
@@ -89,13 +91,18 @@ signal SMSG : std_logic_vector(15 downto 0);
 signal SERR : std_logic_vector(15 downto 0);
 signal SBUSAWORD : std_logic_vector(15 downto 0);
 signal SBUSBWORD : std_logic_vector(15 downto 0);
+signal SModeOrData : std_logic_vector(1 downto 0);
+
+----others----
+signal SDATAWORDLEN: integer range 0 to 32;
+
 begin
 
 ----I/O to Internal Register Buffering----
 SSTR      <= PISTR;     
 SRW       <= PIRW;     
 SCS       <= PICS; 
-SRegAccess <= SCS & SRW & SSTR;
+SRegAccess<= SCS & SRW & SSTR;
 SRegAddr  <= PIRegAddr;
 SCLK      <= PICLK;
 SReset    <= PIMR;    
@@ -103,88 +110,145 @@ SBCSTART  <= PIBCSTART;
 POERROR   <= SError;
 POVALMESS <= SVALMESS;
 POFFEMPTY <= SFFEMPTY;
-PORFLAG   <= SRFLAG;  
-SCMD     <= PICMD;
+SCMD      <= PICMD;
 PORCVA    <= SRCVA;
 PORCVB    <= SRCVB;
 SRTA      <= PIRTA;  
 SRTAP     <= PIRTAP;    
-SRcvFlags <= SBUSB & SBUSA;
 SBUSA     <= PIBUSA;
 SBUSB     <= PIBUSB;  
 SPrtyCheck<= SRTA(4) xnor SRTA(3) xnor SRTA(2) xor SRTA(1) xnor SRTA(0);
 
+SERR(9 downto 0) <= SPRTYERR & PIERR;
+SERR(15 downto 10) <= (others=>'0');
 
-
-    process(PICLK, PIMR, PIRTA, PIRTAP, PIRW, PIRA, PIBCSTART)
+SDATAWORDLEN <= to_integer(SCOMMWORD(4 downto 0)) when SCOMMWORD(4 downto 0)>"00000" else 32;
+  
+process(PICLK, PIMR, PIRTA, PIRTAP, PIRW, PIRA, PIBCSTART)
         begin
             if PIMR='1' then
                 H6110_states <= StIdle;
-                SSTAT     <= x"0000";
-                SCTRL     <= x"0000";
-                SERR      <= x"0000";
+                SSTAT        <= x"0000";
                 SRXFIFO      <= (others=>x"0000");
                 SRCVA        <= '0';
                 SRCVB        <= '0';
+                SVALMESS     <= '0'
+                SDATAWORDLEN <= 0;
+                SModeOrData  <= "00";
 
             elsif rising_edge(PICLK) then
-               
+                SPrevBUSA <= SBUSA;
+                SPrevBUSB <= SBUSB;
 
                 case H6110_states is
-                    when StIdle=>
+                    when StIdle =>
                         if SPrtyCheck=SRTAP then
-                           SERR(9) <= '0';
-                           H6110_states <= StIdle;----degistir 
+                           SPRTYERR <= '0';
+                           H6110_states <= StBusActivity;
                         else
-                           SERR(9) <= '1';
+                           SPRTYERR <= '1';
                            H6110_states <= StIdle; 
                         end if;
-
                     
-                    when StProcessCmd=>
-                        if PICMD(0)(15 downto 11)<"11111" and PICMD(0)(15 downto 11)=SRTA then
-                            if PICMD(0)(9 downto 5)>"00000" and PICMD(0)(9 downto 5)<"11111" then
-                                if PICMD(0)(10)='0' then
-                                    H6110_states<=StSubAddr;
+                    when StBusActivity =>
+                        
+                        if (SBUSA='1' and SPrevBUSA='0') or (SBUSB='1' and SPrevBUSB='0') then
+                            H6110_states <= StValidCmdCheck;
+                            SActA <= '1';
+                            SActB <= '1'; 
+                        else
+                            H6110_states <= StBusActivity;
+                            SActA <= '0';
+                            SActB <= '0'; 
+                        end if;    
+                        
+                            
+                       
+                    when StValidCmdCheck =>
+                        if SERR(1)='1' or SERR(2)='1' then 
+                            if PICMD(0)(15 downto 11)<"11111" and PICMD(0)(15 downto 11)>"00000" then
+                                if PICMD(0)(15 downto 11)=SRTA then
+                                   H6110_states<=StProcessCmd; 
+                                   SSTAT(0) <= '0'; 
+                                   SSTAT(1) <= SActA;
+                                   SSTAT(2) <= SActB; 
+                                   SVALMESS <= '0';
                                 else
-                                    H6110_states<=StStatusSet;
+                                   H6110_states<=StBusActivity;
+                                   SSTAT(0) <= '1'; 
+                                   SSTAT(1) <= '0';
+                                   SSTAT(2) <= '0';
+                                   SVALMESS <= '1';
                                 end if;
                             else
-                                H6110_states<=StModeCode;
+                                H6110_states<=StProcessCmd;
+                                SSTAT(0) <= '0'; 
+                                SSTAT(1) <= SActA;
+                                SSTAT(2) <= SActB; 
+                                SVALMESS <= '0';
                             end if;
                         else
-                            H6110_states<=StBroadcast;
+                            H6110_states<=StBusActivity;
+                            SSTAT(0) <= '1'; 
+                            SSTAT(1) <= '0';
+                            SSTAT(2) <= '0'; 
+                            SVALMESS <= '1';
                         end if;
-                        
 
-                    when StBroadcast=>
+                    when StProcessCmd =>
+                          SCOMMWORD <= PICMD;  
+                          
+                          if PICMD(10)='0' then
+                              SSTAT(5) <= SActA and '1';
+                              SSTAT(6) <= SActB and '1';
+                          else
+                              SSTAT(5) <= '0';
+                              SSTAT(6) <= '0';
+                          end if;
+                    
+                          if PICMD(15 downto 11)="00000" or PICMD(15 downto 11)="11111" then   
+                                H6110_states <= StProcessMode;
+                          else
+                                H6110_states <= StProcessData;
+                          end if;
+   
+                    when StProcessMode=>
                             
-                    when StModeCode=>
 
-                    when StSubAddr=>
-                           case SRcvFlags is
-                                when "10" =>
-                                    SRCVB<='1';
-                                    SRCVA<='0';
-                                    SCOMMWORD <= PICMD;
-                                when "01" =>
-                                    SRCVA<='1';
-                                    SRCVB<='0';  
-                                    SCOMMWORD <= PICMD;
-                                when others =>
-                                    SRCVA<='0';
-                                    SRCVB<='0';
-                                    SCOMMWORD <= SCOMMWORD;
-                            end case;   
-                            H6110_states<=StRcvData;
 
-                    when StRcvData=>
+                    when StProcessData=>
+                          if SERR(5 downto 0) = "000000" then
+                              SRXFIFO <=  PIODATAWORD;
+                              H6110_states <= ; 
+                              if SActA='1' and SActB='0' then
+                                 SBUSAWORD <= PIODATAWORD(SDATAWORDLEN);
+                                 SBUSBWORD <= SBUSBWORD;
+                              elsif SActB='1' and SActA='0' then
+                                 SBUSBWORD <= PIODATAWORD(SDATAWORDLEN);
+                                 SBUSAWORD <= SBUSAWORD;
+                              else
+                                 SBUSBWORD <= SBUSBWORD;
+                                 SBUSAWORD <= SBUSAWORD;
+                              end if;
+                          else
+                              SRXFIFO <= SRXFIFO;
+                              H6110_states <= StBusActivity; 
+                              SBUSBWORD <= SBUSBWORD;
+                              SBUSAWORD <= SBUSAWORD; 
+                          end if;
+                            
+                    when
+                        
+                    when StRcvData =>
                            SRXFIFO      <= PIODATAWORD;
                            H6110_states <= StStatusSet;
                             
-                       
+                           
                             
                     when StStatusSet=>
+            end if;
+end process;
+
 
 process(SRegAccess, SRegAddr, SRiseSTR)
     begin
@@ -195,7 +259,7 @@ process(SRegAccess, SRegAddr, SRiseSTR)
             STXFIFO         <= (others=>x"0000");
             STXSTATWORD     <= x"0000";
             STXMODEDATAWORD <= x"0000";
-        else    
+        else
             case SRegAccess is
 
                 when "000" =>
@@ -255,7 +319,7 @@ process(SRegAccess, SRegAddr, SRiseSTR)
                         elsif SRegAddr="0010" then
                             PIOWORD <= SRXMODEDATAWORD;
                         elsif SRegAddr="0100" then
-                            PIOWORD <= SRXFIFO;
+                            PIOWORD <= SRXFIFO(0);
                         elsif SRegAddr="0101" then
                             PIOWORD <= SSTAT;
                         elsif SRegAddr="0110" then
